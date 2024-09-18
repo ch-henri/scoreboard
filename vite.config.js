cf3d40578e1c0105da44143ee8d5c316991e98b1
deschenaux chh <deschenauxch.h@gmail.com>
2024-09-18 19:40:58 +0200

    Fix pwa offline font not found

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,30 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
     plugins: [
         VitePWA({
-            includeAssets: ['/images/pwa-icon/512.png', '/images/pwa-icon/192.png', '/font/*.woof2', '/gong.mp3' ],
+            includeAssets: [ 'fonts/*.woff2', 'gong.mp3' ],
             manifest: {
                 name: 'Judo Scoreboard',
                 theme_color: '#ffffff',
                 icons: [
                     {
-                      src: "/images/pwa-icon/512.png",
+                    src: "images/favicons/favicon.svg",
+                    type: "image/svg+xml",
+                    sizes: "any"
+                    },
+                    {
+                      src: "images/favicons/512.png",
                       type: "image/png",
                       sizes: "512x512"
                     },
                     {
-                    src: "/images/pwa-icon/192.png",
+                    src: "images/favicons/192.png",
                     type: "image/png",
                     sizes: "192x192"
+                  },
+                  {
+                    src: "images/favicons/apple-touch-icon.png",
+                    type: "image/png",
+                    sizes: "180x180"
                   }
                 ],
                 start_url : "/",
